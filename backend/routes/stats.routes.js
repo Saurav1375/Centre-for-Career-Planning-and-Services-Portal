dd1322c5d410688e4de84e6bd6d8ce7d7c6e2ea9
UtkarshU <umapus@iitbhilai.ac.in>
2026-05-13 16:30:48 +0530

    Remove Unnecessary files

--- backend/routes/stats.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {getAllData , updateData} from '../controllers/stats.controller.js';
-const router = express.Router();
-
-router.get('/', getAllData);
-router.put('/', updateData);
-
-export default router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,28 +13,14 @@ import VerifyEmail from './pages/auth/VerifyEmail';
 import ForgotPassword from './pages/auth/ForgotPassword';
 import ResetPasswordPage from './pages/auth/ResetPasswordPage';
 
-// Student Pages Import
-import Alumni from './pages/student/Alumni';
-import Profile from './pages/student/Profile';
-import Applications from './pages/student/Applications';
-import SavedApplications from './pages/student/Savedapplications';
-import ResumeBuilder from './pages/student/ResumeBuilder';
-import DiscussionForum from './pages/student/DiscussionForum';
-import AnalyticsDashboard from './pages/student/AnalyticsDashboard';
 
 // import Home from './pages/Home';
-import Referrals from './pages/Referrals';
 import Dashboard from './pages/Dashboard';
 
-import AddThread from './components/DiscussionForum/AddThread';
 
 // import HRContactsPage from './pages/student/HRContactsPage';
 
 // Admin Pages Imports
-import AddAlumni from './pages/admin/AddAlumni';
-import CreateJob from './pages/admin/CreateJob';
-import AdminJobList from './pages/admin/AdminJobList';
-import JobApplicants from './pages/admin/JobApplications';
 import CallerDashboard from './pages/student/CallerDashboard';
 
 import CallerHRContacts from './pages/student/CallerHRContacts';
@@ -51,28 +37,21 @@ import AdminCallLog from './pages/admin/AdminCallLog'
 
 function App() {
   const { authUser } = useAuthContext();
-  const { showForgotPassword, showVerifyEmail, showAddThread } = useAppContext();
+  const { showForgotPassword, showVerifyEmail } = useAppContext();
   return (
     <>
 
       {showForgotPassword && <ForgotPassword />}
       {showVerifyEmail && <VerifyEmail />}
-      {showAddThread && <AddThread />}
 
 
       <Routes>
 
         <Route path="/" element={ authUser ? ( authUser.role === "admin" ? ( <AdminDashboardPage /> ) : ( <CallerDashboard /> ) ) : ( <Navigate to="/login" /> ) } />
         {/* <Route path='/' element={authUser ? <Home /> : <Navigate to='/login' />} /> */}
-        <Route path='/profile' element={authUser ? <Profile /> : <Navigate to='/login' />} />
-        <Route path='/discussion-forum' element={authUser ? <DiscussionForum /> : <Navigate to='/login' />} />
         <Route path='/dashboard' element={authUser ? <Dashboard /> : <Navigate to='/login' />} />
         <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
         <Route path='/signup' element={authUser ? <Navigate to='/' /> : <Signup />} />
-        <Route path='/analytics' element={<AnalyticsDashboard />} />
-        <Route path='/referrals' element={<Referrals />} />
-        <Route path='/resumebuilder' element={<ResumeBuilder />} />
-        <Route path='/alumni' element={<Alumni />} />
         <Route path='/hr-contacts' element={<CallerHRContacts />} />
         <Route path='/caller-dashboard' element={<CallerDashboard />} />
         <Route path='/caller-profile' element={<CallerProfile />} />
@@ -86,39 +65,8 @@ function App() {
         <Route path='/admin/hr-call-log' element={authUser?.role === 'admin' ? <AdminCallLog /> : <Navigate to='/' />} />s
 
         {!authUser && <Route path='/reset-password/:resetToken' element={<ResetPasswordPage />} />}
-        <Route
-          path="/admin/jobs"
-          element={authUser?.role === "admin" ? <AdminJobList /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/admin/job/:jobId/applicants"
-          element={authUser?.role === "admin" ? <JobApplicants /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/applications"
-          element={
-            authUser
-              ? <Applications />
-              : <Navigate to="/login" replace />
-          }
-        />
-        <Route
-          path="/saved-applications"
-          element={
-            authUser
-              ? <SavedApplications />
-              : <Navigate to="/login" replace />
-          }
-        />
-        <Route
-          path="/admin/create-job"
-          element={authUser?.role === "admin" ? <CreateJob /> : <Navigate to="/" />}
-        />
-        {/* ALUMNI PAGES */}
-        <Route
-          path="/admin/add-alumni"
-          element={authUser?.role === 'admin' ? <AddAlumni /> : <Navigate to='/' />}
-        />
+
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
 
